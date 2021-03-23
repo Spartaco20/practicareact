3020785129756dc4760f01024887847e84d637aa
Spartaco20 <71533881+Spartaco20@users.noreply.github.com>
2021-03-23 12:25:25 -0500

    Todos los botones

--- src/App.js
+++ src/App.js
@@ -1,24 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  altura: {
+    paddingTop: 80,
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Freddy Alexander Florez Tibata</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed className={classes.altura}>
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} >
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  <form className={classes.root} noValidate autoComplete="off">
+                    <TextField id="outlined-basic" fullWidth label="Ingrese un valor" variant="outlined" />
+                  </form>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="secondary">
+                    AC
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined">
+                    CE
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    %
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    /
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    7
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    8
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    9
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    X
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+            
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    4
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    5
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    6
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    -
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    1
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    2
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    3
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    +
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+
+              <Grid item xs={2}></Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    0
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    .
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={4}>
+                <Paper className={classes.paper}>
+                  <Button variant="outlined" color="primary">
+                    =
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={2}></Grid>
+            </Grid>
+          </div>
+        </Typography>
+      </Container>
+    </React.Fragment>
   );
 }
 
